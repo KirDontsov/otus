f7d650ffcd4e2c1e52a66cb2ebf09f5093efc415
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2022-06-23 15:08:20 +0300

    fix: task-1
    TASK-1

---
+++ src/maxItemAssociation/__tests__/maxItemAssociation.test.tsx
@@ -0,0 +1,50 @@
+import { maxItemAssociation } from '../index';
+
+const input: string[][] = [
+  ['a', 'b'],
+  ['a', 'c'],
+  ['d', 'e'],
+];
+
+const input2: string[][] = [
+  ['q', 'w', 'a'],
+  ['a', 'b'],
+  ['a', 'c'],
+  ['q', 'e'],
+  ['q', 'r'],
+];
+
+const input3: string[][] = [
+  ['d', 'm'],
+  ['a', 'b'],
+  ['a', 'c'],
+  ['d', 'e'],
+];
+
+const input4: string[][] = [
+  ['d', 'm'],
+  ['a', 'b'],
+  ['a', 'c'],
+  ['d', 'e'],
+  ['d', 'j'],
+];
+
+describe('maxItemAssociation', () => {
+  test('maxItemAssociation with input', () => {
+    const result = maxItemAssociation(input);
+    expect(result).toEqual(['a', 'b', 'c']);
+  });
+
+  test('maxItemAssociation with input2', () => {
+    const result = maxItemAssociation(input2);
+    expect(result).toEqual(['a', 'b', 'c', 'e', 'q', 'r', 'w']);
+  });
+  test('maxItemAssociation with input3', () => {
+    const result = maxItemAssociation(input3);
+    expect(result).toEqual(['a', 'b', 'c']);
+  });
+  test('maxItemAssociation with input4', () => {
+    const result = maxItemAssociation(input4);
+    expect(result).toEqual(['a', 'b', 'c', 'd', 'e', 'j', 'm']);
+  });
+});
--- src/maxItemAssociation/maxItemAssociation.ts
+++ src/maxItemAssociation/maxItemAssociation.ts
@@ -6,12 +6,12 @@ const input: string[][] = [
   ['q', 'r'],
 ];
 
-function getUnique<T extends string>(array: T[]): string[] {
-  return array.filter((item: T, i: number) => array.indexOf(item) === i);
+function getUnique<T>(array: T[]): T[] {
+  return array.filter((item, i) => array.indexOf(item) === i);
 }
 
-function getDuplicates<T extends string>(array: T[]): string[] {
-  return array.filter((item: T, i: number) => array.indexOf(item) !== i);
+function getDuplicates<T>(array: T[]): T[] {
+  return array.filter((item, i) => array.indexOf(item) !== i);
 }
 
 export function maxItemAssociation(arr: string[][]): string[] {
@@ -27,6 +27,12 @@ export function maxItemAssociation(arr: string[][]): string[] {
       }
     });
   });
+
+  if (getUnique(rawResult.flat()).length % 2 === 0) {
+    return getUnique(rawResult.flat())
+      .sort()
+      .slice(0, getUnique(rawResult.flat()).length / 2);
+  }
 
   return getUnique(rawResult.flat()).sort();
 }
